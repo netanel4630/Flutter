2bc36e634a2cb325f65c3c4e1225f8eb35439213
Netanel Hajaj <Netanel.4630@gmail.com>
2021-06-27 11:04:02 +0300

    first app

--- myaap/lib/main.dart
+++ myaap/lib/main.dart
@@ -8,23 +8,83 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.amber,
         appBar: AppBar(
-          title: Text("My First App"),
+          title: Text("Ninja ID"),
           centerTitle: true,
-          backgroundColor: Colors.amberAccent,
+          backgroundColor: Colors.amberAccent[700],
         ),
-        body: Center(
-          child: TextButton.icon(
-            icon: Icon(Icons.mail),
-            onPressed: null,
-            label: Text(""),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/n.jpeg'),
+                  radius: 60.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey,
+              ),
+              Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                'Chun-Li',
+                style: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            SizedBox(height: 30.0),
+              Text(
+                'Currect Ninja level',
+                style: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                '8',
+                style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    'chun.li@gmail.com',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                  )
+                ],
+              )
+            ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Text("Hello"),
-          backgroundColor: Colors.amberAccent,
-        )
     );
   }
 }
