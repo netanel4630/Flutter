752f3f8f9151481c41fb2368872d027f7b761d7d
Netanel Hajaj <Netanel.4630@gmail.com>
2021-07-01 08:16:02 +0300

    /

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,10 +7,30 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void setupWorldTime() async {
+     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+     await instance.getTime();
+     Navigator.pushReplacementNamed(context, '/home', arguments: {
+       'location': instance.location,
+       'flag': instance.flag,
+       'time': instance.time,
+     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text('Loading..'),
+      ),
     );
   }
 }
