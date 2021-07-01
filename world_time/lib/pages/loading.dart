478bdd313a4f9966e9138dfc2fceec47116d2928
Netanel Hajaj <Netanel.4630@gmail.com>
2021-07-01 12:47:43 +0300

    finished project

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
-
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
@@ -15,6 +15,7 @@ class _LoadingState extends State<Loading> {
        'location': instance.location,
        'flag': instance.flag,
        'time': instance.time,
+       'isDaytime': instance.isDaytime,
      });
   }
 
@@ -27,10 +28,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text('Loading..'),
-      ),
+      backgroundColor: Colors.blue[900],
+      body:Center(
+        child: SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      )
     );
   }
 }
--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 //'https://worldtimeapi.org/api/timezone/Asia/Jerusalem'
 
 class WorldTime {
@@ -7,8 +8,9 @@ class WorldTime {
   String time = ''; //the time in that location
   String flag = ''; //url to an asset flag icon
   String url = ''; //location url for api endpoint
+  bool isDaytime; //true day; false night
 
-  WorldTime({required this.location, required this.flag, required this.url});
+  WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
     try {
@@ -30,7 +32,8 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
       //set the time property
-      time = now.toString();
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
+      time = DateFormat.jm().format(now);
     }
     catch (e) {
       print('caught error: $e');
